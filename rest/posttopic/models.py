182abfca4aa136cc62598d1d730f4f31706bc93d
Razin <raz.issa00@gmail.com>
2021-01-19 16:21:54 +0600

    Delete models.py

--- rest/posttopic/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class send_command(models.Model):
-    trxid = models.CharField(max_length=100)
-    command = models.CharField(max_length=20)
-    topic = models.CharField(max_length=20)
-    message = models.JSONField()
