2656ba660a17489aea3191bd316668d19e839110
Razin <raz.issa00@gmail.com>
2021-01-19 16:21:57 +0600

    Delete serializers.py

--- rest/posttopic/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import send_command
-
-class send_commandSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = send_command
-        fields = "__all__"
