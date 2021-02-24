9b3fd53e3b9aea7943115c1688cc11cf8501195f
Razin <raz.issa00@gmail.com>
2021-02-24 13:43:23 +0600

    Update urls.py

--- rest/posttopic/urls.py
+++ rest/posttopic/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.send_commandRequestList,name="send-command-request-list"),
     path('commandList/', views.send_commandList,name="command-list"),
     path('commandDetail/<str:pk>/', views.send_commandDetail,name="command-detail"),
-    path('commandCreate/', views.send_commandCreate,name="command-create"),
+    path('send_command/', views.send_commandCreate,name="send-command"),
     path('commandUpdate/<str:pk>/', views.send_commandUpdate,name="command-update"),
     path('commandDelete/<str:pk>/', views.send_commandDelete,name="switch-delete"),
 ]
